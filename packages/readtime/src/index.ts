b5030446ae3f6ad4c79cf67f2c7cc86c7abeaad2
Abhinav Saxena <abhinav061@gmail.com>
2026-09-23 08:11:34 +0530

    chore(readtime): bump to 0.2.1, changeset, log Track B findings

--- packages/readtime/src/index.ts
+++ packages/readtime/src/index.ts
@@ -19,7 +19,7 @@ export function readtimePlugin(config?: ReadtimeConfig): PluginDescriptor {
 
 	return {
 		id: "readtime",
-		version: "0.1.0",
+		version: "0.2.1",
 		format: "standard",
 		entrypoint: "@plugdash/readtime/sandbox",
 		capabilities: ["content:read", "content:write"],
